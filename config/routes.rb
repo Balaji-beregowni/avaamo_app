eb7c0d120c9d544378746895c03ef709a32d5a2b
Balaji Beregowni <balaji.beregowni@peoplefluent.com>
2024-03-06 07:40:26 +0530

    implemented FE changes using react

--- config/routes.rb
+++ config/routes.rb
@@ -3,34 +3,33 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
 
   mount Sidekiq::Web => '/sidekiq'
-  resources  :users,only: [:index] do
-    member do
-      get 'orders', to: 'orders#download', defaults: { format: 'csv' }
-      post 'download_orders_csv'
+  namespace :api do
+    resources  :users,only: [:index] do
+      member do
+        post 'download_orders_csv'
+      end
+      collection do
+        post 'import'
+      end
     end
-    collection do
-      post 'import'
-    end
-  end
 
-  resources  :products,only: [:index] do
-    collection do
-      post 'import'
+    resources  :products,only: [:index] do
+      collection do
+        post 'import'
+      end
     end
-  end
 
-  resources  :attachments,only: [:index] do
-    member do
-      get 'csv_status'
+    resources  :attachments,only: [:index] do
+      member do
+        get 'csv_status'
+      end
     end
-  end
 
-  resources  :orders do
-    member do
-      get 'csv_status'
-    end
-    collection do
-      post 'import'
+    resources  :orders do
+      collection do
+        post 'import'
+      end
     end
   end
 end
+
